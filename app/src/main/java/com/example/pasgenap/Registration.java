44ab7ba682dabe23bc8cd93f82d4c45cbcff1916
AkbarFelda <akbarfelda65@gmail.com>
2023-05-23 19:55:51 +0700

    style : [AF] add registerpage

--- app/src/main/java/com/example/pasgenap/Registration.java
+++ app/src/main/java/com/example/pasgenap/Registration.java
@@ -1,12 +1,11 @@
 package com.example.pasgenap;
-import android.content.Intent;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,46 +16,53 @@ import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     public static final String SHARED_PREFS = "shared_prefs";
     public static final String EMAIL_KEY = "email_key";
     public static final String PASSWORD_KEY = "password_key";
 
     SharedPreferences sharedpreferences;
+    EditText txtFullName;
     EditText txtUsername;
+    EditText txtEmail;
     EditText txtPassword;
-    Button btnLogin;
+    Button btnRegister;
     ProgressBar pbLoadingBar;
-    TextView btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
 
         sharedpreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
 
         // Mendapatkan referensi
+        txtFullName = findViewById(R.id.txtFullName);
         txtUsername = findViewById(R.id.txtUsername);
+        txtEmail = findViewById(R.id.txtEmail);
         txtPassword = findViewById(R.id.txtPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        pbLoadingBar = findViewById(R.id.pbLoadingBar);
         btnRegister = findViewById(R.id.btnRegister);
+        pbLoadingBar = findViewById(R.id.pbLoadingBar);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String fullName = txtFullName.getText().toString();
                 String username = txtUsername.getText().toString();
+                String email = txtEmail.getText().toString();
                 String password = txtPassword.getText().toString();
+
 
                 // untuk set progress bar
                 pbLoadingBar.setVisibility(View.VISIBLE);
-                btnLogin.setEnabled(false);
+                btnRegister.setEnabled(false);
 
-                //hit API
-                AndroidNetworking.post("https://mediadwi.com/api/latihan/login")
+                AndroidNetworking.post("https://mediadwi.com/api/latihan/register-user")
+                        .addBodyParameter("full_name", fullName)
                         .addBodyParameter("username", username)
+                        .addBodyParameter("email", email)
                         .addBodyParameter("password", password)
                         .setPriority(Priority.MEDIUM)
                         .build()
@@ -64,49 +70,31 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(JSONObject response) {
                                 pbLoadingBar.setVisibility(View.GONE);
-                                btnLogin.setEnabled(true);
+                                btnRegister.setEnabled(true);
 
                                 try {
                                     boolean status = response.getBoolean("status");
                                     String message = response.getString("message");
                                     if (status) {
-                                        // Contoh menyimpan email ke SharedPreferences
-                                        SharedPreferences.Editor editor = sharedpreferences.edit();
-                                        editor.putString(EMAIL_KEY, username);
-                                        editor.putString(PASSWORD_KEY, password);
-                                        editor.apply();
-
-                                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                                        // Registrasi berhasil, lakukan tindakan sesuai kebutuhan
+                                        Toast.makeText(Registration.this, message, Toast.LENGTH_SHORT).show();
                                     } else {
-                                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(Registration.this, message, Toast.LENGTH_SHORT).show();
                                     }
                                 } catch (Exception e) {
-                                    Toast.makeText(MainActivity.this, "Failed to login", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(Registration.this, "Failed to register", Toast.LENGTH_SHORT).show();
                                 }
                             }
 
                             @Override
                             public void onError(com.androidnetworking.error.ANError error) {
                                 pbLoadingBar.setVisibility(View.GONE);
-                                btnLogin.setEnabled(true);
+                                btnRegister.setEnabled(true);
 
-                                Toast.makeText(MainActivity.this, "Failed to login", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(Registration.this, "Failed to register", Toast.LENGTH_SHORT).show();
                             }
                         });
             }
         });
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerClicked(view);
-            }
-        });
-    }
-
-    public void registerClicked(View view) {
-        // Pindah ke halaman Register Page
-        Intent intent = new Intent(MainActivity.this, Registration.class);
-        startActivity(intent);
     }
 }
